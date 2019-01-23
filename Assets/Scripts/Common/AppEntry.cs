8f5d0a493d07a87ba5983cfd8dc26c2576ad5b0b
orange634nty <orange634nty@gmail.com>
2019-01-24 00:44:16 +0900

    update
    - add singlton class
    - load singlton from AppEntry
    - create SoundManager class
    - fix some codes.

---
+++ Assets/Scripts/Common/AppEntry.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppEntry : MonoBehaviour
+{
+    // [SerializeField] private GameObject SoundManagerPref;
+    // [SerializeField] private GameObject dataStorePref;
+
+    [RuntimeInitializeOnLoadMethod]
+    static void Initialize()
+    {
+        // シングルトンのロード
+        Instantiate(Resources.Load("Prefabs/DataStore") as GameObject);
+        Instantiate(Resources.Load("Prefabs/SoundManager") as GameObject);
+        // Instantiate(SoundManagerPref);
+        // Instantiate(dataStorePref);
+    }
+}
