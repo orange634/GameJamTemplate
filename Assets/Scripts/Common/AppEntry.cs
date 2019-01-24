24d93c72f916719ed24cc516a9e6085208f633de
orange634nty <orange634nty@gmail.com>
2019-01-25 00:06:23 +0900

    fix some code.

--- Assets/Scripts/Common/AppEntry.cs
+++ Assets/Scripts/Common/AppEntry.cs
@@ -4,16 +4,11 @@ using UnityEngine;
 
 public class AppEntry : MonoBehaviour
 {
-    // [SerializeField] private GameObject SoundManagerPref;
-    // [SerializeField] private GameObject dataStorePref;
-
-    [RuntimeInitializeOnLoadMethod]
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     static void Initialize()
     {
         // シングルトンのロード
         Instantiate(Resources.Load("Prefabs/DataStore") as GameObject);
         Instantiate(Resources.Load("Prefabs/SoundManager") as GameObject);
-        // Instantiate(SoundManagerPref);
-        // Instantiate(dataStorePref);
     }
 }
